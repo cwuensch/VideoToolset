5914b003282150a15ced53c33527568843ccf55d
Christian Wünsch <c.wuensch@gmail.com>
2022-03-23 12:58:14 +0100

    VirtualDub-Paramter nur für Ausgabe nutzen

--- Start_vdub.cmd
+++ Start_vdub.cmd
@@ -1,3 +1,8 @@
-net use I: %STORAGE_IN_PATH% /user:%STORAGE_IN_USER% %STORAGE_IN_PASSWORD%
-net use O: %STORAGE_OUT_PATH% /user:%STORAGE_OUT_USER% %STORAGE_OUT_PASSWORD%
+if not "%STORAGE_IN_PATH%"=="" (
+  net use I: %STORAGE_IN_PATH% /user:%STORAGE_IN_USER% %STORAGE_IN_PASSWORD%
+)
+if not "%STORAGE_OUT_PATH%"=="" (
+  net use O: %STORAGE_OUT_PATH% /user:%STORAGE_OUT_USER% %STORAGE_OUT_PASSWORD%
+)
+
 "C:\Program Files (x86)\VirtualDub\vdub.exe" %*
